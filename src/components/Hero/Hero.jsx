5acef210b0218a38cfdd17ad1b2cfbe4275ae66a
Galib Mia <gm71bd@gmail.com>
2024-06-05 10:43:47 +0600

    Home page hero section added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroImage from "../../assets/image/hero.png";
 import './Hero.css';
+import { AcademicCapIcon, ComputerDesktopIcon, PlayCircleIcon } from '@heroicons/react/24/solid'
 
 const Hero = () => {
     return (
@@ -12,10 +13,16 @@ const Hero = () => {
                 </p>
                 <button type="button" className="button-join outline-none font-medium rounded-lg px-8 py-4 text-center text-lg me-2 mb-2">Join For Free</button>
                 <button type="button" className="button-course outline-none font-medium rounded-lg px-8 py-4 text-center text-lg me-2 mb-2">Find Courses</button>
+                <div className='my-5 flex gap-5'>
+                    <p className='flex gap-2 text-white'><AcademicCapIcon className="size-6 text-white" /><span>Over 12 million students</span></p>
+                    <p className='flex gap-2 text-white'><PlayCircleIcon className="size-6 text-white" /><span>More than 60,000 courses</span></p>
+                    <p className='flex gap-2 text-white'><ComputerDesktopIcon className="size-6 text-white" /><span>Learn anything online</span></p>
+                </div>
             </div>
             <div className='w-1/2'>
-                <img className='mx-auto' src={HeroImage} alt="Image" />
+                <img className='mx-auto hero-img' src={HeroImage} alt="Image" />
             </div>
+            
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./Home.css"
 import Hero from '../Hero/Hero';
+import bgImg from "../../assets/logo/hero-bg.png"
 
 const Home = () => {
     return (
         <div>
-            <div className='hero-bg'>
+            <div className='hero-bg-img mt-14'>
             <Hero></Hero>
             </div>
         </div>
